dcecf541626ee52fbb153cca10640ab3baa89ea2
kamal20247 <147083931+kamalgurjar7@users.noreply.github.com>
2024-11-13 20:44:35 +0530

    add register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -22,7 +22,7 @@ const userSchema = new Schema (
             trim : true ,
            
         } ,
-        fullname : {
+        fullName : {
             type:String ,
             required : true , 
             
@@ -62,7 +62,7 @@ userSchema.pre("save" , async function (next) {
     if(!this.isModified("password")) return next();
 
     
-    this.password=bcrypt.hash(this.password , 10)
+    this.password= await bcrypt.hash(this.password , 10)
     next()
 })
 
@@ -76,7 +76,7 @@ userSchema.methods.generateAccessToken = function () {
             _id:this._id,
             email:this.email,
             username:this.username,
-            fullname:this.fullname, 
+            fullName:this.fullName, 
         }
         ,
         process.env.ACCESS_TOKEN_SECRET,
